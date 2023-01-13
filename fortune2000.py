a7283084d21bfeb52e0f52cffd99cd665b60e7d0
Eric Kopen <33678196+ekopen@users.noreply.github.com>
2023-01-13 11:03:01 -0600

    Update fortune2000.py

--- fortune2000.py
+++ fortune2000.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# read in the data
 pd.read_csv(r'C:\Users\ekopen\Documents\Kaggle\Forbes_2000_top_company_CLNQ11.csv').to_pickle("./fortune2000.pkl")
 df = pd.read_pickle("fortune2000.pkl")
 
@@ -10,26 +11,34 @@ for x in range(len(df.index)):
     if df.iloc[x,10][-1] == "]":
         df.iloc[x,10] = df.iloc[x,10][:-1]
 
+# converting some columns to float values so they can be used in formulas
 calcColumns = ['Revenue (Billions)','Profits (Billions)','Market Value (Billions)','Total Employees']
 for x in calcColumns:
     df[x] = pd.to_numeric(df[x])
 
-#attempting to filter out holding companies with low employee count
-dffiltered = df[(df['Total Employees']>1000)]
+#filtering out holding companies with low employee count (these have weird metrics that skew the analysis)
+# and any non-US companies (not sure what their reporting standards are)
+dffiltered = df[(df['Total Employees']>1000) & (df['Country']=='United States')]
 
-
+# establishing columns to be used in calculations and performing the calc below
 calcColumns = calcColumns[:-1]
 newColumns = ['Revenue per Employee', 'Profits per Employee', 'Market Value per Employee']
-
 for x,y in zip(newColumns,calcColumns):
     dffiltered[x] = (dffiltered[y] / (df['Total Employees'])) * 1000000000
 
-print(dffiltered.info())
+# checking to make things look right
+# print(dffiltered.info())
+print(dffiltered)
 
-dfsorted = dffiltered.sort_values(by=['Profits per Employee'],ascending=False)
+#SOMETHING ISNT WORKING!!!!!!
+dfsummary = dffiltered[['Organization Name', 'Industry', 'Total Employees', 'Profits (Billions)', 'Revenue per Employee',
+                      'Profits per Employee', 'Market Value per Employee']].groupby([
+                        'Industry']).sum(['Revenue per Employee','Total Employees', 'Profits (Billions)',
+                                          'Profits per Employee', 'Market Value per Employee']).sort_values(by=[
+                                            'Profits per Employee'],ascending=False)
 
 pd.options.display.float_format = '${:0,.0f}'.format
-print(dfsorted[['Organization Name','Revenue per Employee','Total Employees', 'Profits per Employee', 'Market Value per Employee']])
+print(dfsummary)
 
 # df.plot(kind='bar', x='Date', y='Random Walk')
 # plt.show()
