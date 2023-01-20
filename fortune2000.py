2c1b996f56b2da5a837021f52b782db6c8dcb563
Eric Kopen <33678196+ekopen@users.noreply.github.com>
2023-01-20 15:20:51 -0600

    Update fortune2000.py

--- fortune2000.py
+++ fortune2000.py
@@ -7,7 +7,7 @@ pd.options.display.float_format = '${:0,.0f}'.format
 pd.read_csv(r'C:\Users\ekopen\Documents\Kaggle\Forbes_2000_top_company_CLNQ11.csv').to_pickle("./fortune2000.pkl")
 df_original = pd.read_pickle("fortune2000.pkl")
 
-def data_clean(df):
+def data_clean_filter(df):
     # there was an issue with weird brackets in the employee column (column 10) so getting ride of these
     for x in range(len(df.index)):
         if df.iloc[x,10][-1] == "]":
@@ -16,17 +16,15 @@ def data_clean(df):
     dirty_Columns = ['Revenue (Billions)','Profits (Billions)','Market Value (Billions)','Total Employees']
     for x in dirty_Columns:
         df[x] = pd.to_numeric(df[x])
-    df_cleaned = df
-    return df_cleaned
-
-def data_filter(df):
     #filtering out holding companies with low employee count (these have weird metrics that skew the analysis)
     # and any non-US companies (not sure what their reporting standards are)
     # also grouping by industry
-    df_filtered = df[(df['Total Employees']>1000) & (df['Country']=='United States')]
-    return df_filtered
+    df = df[(df['Total Employees']>1000) & (df['Country']=='United States')]
+    return df
 
 def analysis1(df):
+    # attempting to analyze financial metrics by employee over each industry
+    # group by industry
     df = df[['Industry','Revenue (Billions)','Profits (Billions)','Market Value (Billions)',
          'Total Employees']].groupby('Industry').agg('sum')
     # establishing columns to be used in calculations and performing the calc below
@@ -35,15 +33,17 @@ def analysis1(df):
     for x,y in zip(newColumns,calc_Columns):
         df[x] = (df[y] / (df['Total Employees'])) * 1000000000
     df = df.sort_values(by=['Profits per Employee'], ascending=False)
-    df_analysis1 = df
-    return df_analysis1
+    return df
 
-df_grouped_analysis = analysis1(data_filter(data_clean(df_original)))
-df_top_one_analysis = 1
+def analysis2(df, top_industry):
+    df = df.filter(items = ['Diversified Financials'], axis=0)
+    return df
+
+df_cleaned_filtered = data_clean_filter(df_original)
+df_grouped_analysis = analysis1(df_cleaned_filtered)
+top_industry = df_grouped_analysis.head(1)
+df_top_industry = analysis2(df_cleaned_filtered, top_industry)
 
 
 
-
-# top5 = dffinal.head(5)
-# print(top5.index.values)
 
