1869c50f07e7c6a601a95c68ba0f31219a15422d
Eric Kopen <33678196+ekopen@users.noreply.github.com>
2023-01-30 14:51:48 -0600

    Update fortune2000.py

--- fortune2000.py
+++ fortune2000.py
@@ -13,7 +13,7 @@ def data_clean_filter(df):
     # there was an issue with weird brackets in the employee column (column 10) so getting ride of these
     for x in range(len(df.index)):
         if df.iloc[x,10][-1] == ']':
-            df.iloc[x,10] = df.iloc[x,10][:-1]
+            df  .iloc[x,10] = df.iloc[x,10][:-1]
     # converting some columns to float values so they can be used in formulas
     dirty_Columns = ['Revenue (Billions)','Profits (Billions)','Market Value (Billions)','Total Employees']
     for x in dirty_Columns:
@@ -68,56 +68,52 @@ def analysis1(df):
     # attempting to analyze financial metrics by employee over each industry
     # group by industry
     # the industry nickname is throwing an error here.... i think it is based on how i renamed it? Not sure
-    df = df[['Industry','Industry Nickname','Revenue (Billions)','Profits (Billions)','Market Value (Billions)',
-         'Total Employees']].groupby('Industry Nickname').agg('sum')
-    df = calculate_columns(df)
-    df = df.sort_values(by=['Profits per Employee'], ascending=False).reset_index()
-    return df
+    dfa1 = df[['Industry','Industry Nickname','Revenue (Billions)','Profits (Billions)','Market Value (Billions)',
+         'Total Employees']].groupby('Industry Nickname').agg('sum', numeric_only=True)
+    dfa1 = calculate_columns(dfa1)
+    dfa1 = dfa1.sort_values(by=['Profits per Employee'], ascending=False).reset_index()
+    return dfa1
 
 def analysis2(df, industry):
     #creating a way to drill down into certain industries
-    df = df[df['Industry'] == industry]
-    df = calculate_columns(df)
-    df = df.sort_values(by=['Profits per Employee'], ascending=False)
-    return df
+    dfa2 = df[df['Industry'] == industry]
+    dfa2 = calculate_columns(dfa2)
+    dfa2 = dfa2.sort_values(by=['Profits per Employee'], ascending=False)
+    return dfa2
 
-def analysis3_v2(df):
+def analysis3(df):
     # top # of companies by industry
     dfgrouped = analysis1(df)
     num_top_companies = 5
     #get top 'x'' companies by industry and add some calculated columns
-    df = df[['Organization Name','Industry Nickname','Revenue (Billions)','Profits (Billions)','Market Value (Billions)',
+    dfa3 = df[['Organization Name','Industry Nickname','Revenue (Billions)','Profits (Billions)','Market Value (Billions)',
          'Total Employees']].groupby('Industry Nickname').head(num_top_companies)
-    df = calculate_columns(df)
+    dfa3 = calculate_columns(df)
 
     dfgrouped = dfgrouped['Industry Nickname'].to_dict()
     dfgrouped_inv = {v: k for k, v in dfgrouped.items()}
 
-    df['Industry Rank'] = df['Industry Nickname'].map(dfgrouped_inv)
-    df = df.sort_values(by=['Industry Rank','Profits per Employee'], ascending=[True, False])
+    dfa3['Industry Rank'] = dfa3['Industry Nickname'].map(dfgrouped_inv)
+    df_a3 = dfa3.sort_values(by=['Industry Rank','Profits per Employee'], ascending=[True, False])
 
-    return df
+    return df_a3
 
 
 
 df_cleaned_filtered = data_clean_filter(df_original)
 
-# DF_grouped_analysis = analysis1(df_cleaned_filtered)
-DF_grouped_analysis_2 = analysis3_v2(df_cleaned_filtered)
+DF_grouped_analysis = analysis1(df_cleaned_filtered)
+DF_grouped_analysis_2 = analysis3(df_cleaned_filtered)
 
-print(DF_grouped_analysis_2)
-
-# for x in range(len(DF_grouped_analysis.index)):
-#     print(DF_grouped_analysis.index.tolist()[x])
-# print('The five MOST profitable industries per employee are: ' +  str((DF_grouped_analysis.index.tolist()[0:5])))
-# print('The five LEAST profitable industries per employee are: ' +  str((DF_grouped_analysis.index.tolist()[-5:])))
+print('The five MOST profitable industries per employee are: ' +  str((DF_grouped_analysis
+                                                                       ['Industry Nickname'].tolist()[0:5])))
+print('The five LEAST profitable industries per employee are: ' +  str((DF_grouped_analysis
+                                                                        ['Industry Nickname'].tolist()[-5:])))
 
 # bar = DF_grouped_analysis['Profits per Employee'].plot(x='Industry', y='Profit/Employee', kind='bar')
 
-# df = pd.DataFrame({'lab':['A', 'B', 'C'], 'val':[10, 30, 20]})
-# ax = df.plot.bar(x='lab', y='val', rot=0)
-
-
+#
+#
 # print('Select one industry to retrieve a dataframe for: ')
 # industry_specifier = input()
 # DF_detailed_industry_analysis = analysis2(df_cleaned_filtered, industry_specifier)
