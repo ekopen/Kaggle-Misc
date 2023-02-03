6368caaadc3e6d6e28bed15657c09c32d61977d9
Eric Kopen <33678196+ekopen@users.noreply.github.com>
2023-02-03 16:58:13 -0600

    Update F500_yearly_finances.py

--- F500_yearly_finances.py
+++ F500_yearly_finances.py
@@ -1,1 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+# find the dataset source here:
+# https://www.kaggle.com/datasets/yashsrivastava51213/revenue-and-profit-of-fortune-500-companies?select=fortune500.csv
+pd.read_csv(r'C:\Users\ekopen\Documents\Kaggle\fortune500.csv').to_pickle('./fortune500.pkl')
+df_original = pd.read_pickle('fortune500.pkl')
+
+# print(df_original.keys())
+
+df_grouped = df_original[['Year','Revenue (in millions)','Profit (in millions)']].groupby('Year').agg('sum')
+
+x = df_grouped['Year']
+y1 = df_grouped['Revenue (in millions']
+# y2 = df_grouped['Profit (in millions']
+
+plt.plot(x, y1)
+plt.xlabel("X-axis")  # add X-axis label
+plt.ylabel("Y-axis")  # add Y-axis label
+plt.title("Any suitable title")  # add title
+plt.show()
