cfe651c15e2cf572d45aca58d17687507c35dda4
Eric Kopen <33678196+ekopen@users.noreply.github.com>
2023-02-08 16:37:40 -0600

    Update F500_yearly_finances.py

--- F500_yearly_finances.py
+++ F500_yearly_finances.py
@@ -4,34 +4,41 @@ import matplotlib.pyplot as plt
 
 # find the dataset source here:
 # https://www.kaggle.com/datasets/yashsrivastava51213/revenue-and-profit-of-fortune-500-companies?select=fortune500.csv
-pd.read_csv(r'C:\Users\ekopen\Documents\Kaggle\fortune500.csv').to_pickle('./fortune500.pkl')
-df = pd.read_pickle('fortune500.pkl')
+# pd.read_csv(r'C:\Users\ekopen\Documents\Kaggle\fortune500.csv').to_pickle('./fortune500.pkl')
+# df = pd.read_pickle('fortune500.pkl')
+#
+# df = df[df['Rank'] <= 500]
+# df['Revenue (in millions)'] = pd.to_numeric(df['Revenue (in millions)'].str.replace(',',''))
+# df['Profit (in millions)'] = df['Profit (in millions)'].str.replace(',','')
+# df['Profit Margin'] = 0
+# average_pm = .08
+# for x in range(len(df.index)):
+#     if df.iloc[x,4] != 'N.A.':
+#         df.iloc[x,5] = pd.to_numeric(df.iloc[x,4])/df.iloc[x,3]
+#     else:
+#         df.iloc[x,5] = average_pm
+# df['Profit (in millions)'] = df['Revenue (in millions)'] * df['Profit Margin']
+# df.to_pickle('./cleaneddf.pkl')
 
-df = df[df['Rank'] <= 500]
-average_pm = .08
-df['Profit Margin'] = 0
-
-df['Revenue (in millions)'] = pd.to_numeric(df['Revenue (in millions)'].str.replace(',',''))
-
-df['Profit (in millions)'] = df['Profit (in millions)'].str.replace(',','')
-
-for x in range(len(df.index)):
-    if df.iloc[x,4] != 'N.A.':
-        df.iloc[x,5] = pd.to_numeric(df.iloc[x,4])/df.iloc[x,3]
-    else:
-        df.iloc[x,5] = average_pm
-
-df['Profit (in millions)'] = df['Revenue (in millions)'] * df['Profit Margin']
-df_focus = df[df['Year'] == 2003].sort_values(by=['Profit (in millions)'], ascending=True)
+df = pd.read_pickle(r"C:\Users\ekopen\PycharmProjects\pythonProject\cleaneddf.pkl")
+df_focus = df[(df['Year'] >= 2001) & (df['Year'] <= 2001)]
 dfgrouped = df.groupby('Year').agg('sum').reset_index()
 dfgrouped['Profit Margin'] = dfgrouped['Profit (in millions)'] / dfgrouped['Revenue (in millions)']
 
 df_dict = {}
+year_dict = {}
 
 for x in dfgrouped['Year']:
-    df_dict[x] = df[df['Year'] == x]
+    year_dict['DF'] = df[df['Year'] == x]
+    year_dict['Average PM'] = np.average(year_dict['DF']['Profit Margin'], weights=year_dict['DF']['Revenue (in millions)'])
+    year_dict['Std Dev PM'] = np.std(year_dict['DF']['Profit Margin'])
+    year_dict['Upper Bound'] = (year_dict['Std Dev PM'] + year_dict['Std Dev PM'] * 3)
+    year_dict['Lower Bound'] = (year_dict['Std Dev PM'] + year_dict['Std Dev PM'] * -3)
+    df_dict[x] = year_dict
+    year_dict = {}
 
-print(df_dict)
+for x in df_dict:
+    print(df_dict)
 
 # x2 = df['Year']
 # y3 = df['Profit Margin']
