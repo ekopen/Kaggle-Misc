012528c67e705a357aad516f6593951b3d3a022a
Eric Kopen <33678196+ekopen@users.noreply.github.com>
2023-02-10 14:35:06 -0600

    Update F500_yearly_finances.py

--- F500_yearly_finances.py
+++ F500_yearly_finances.py
@@ -22,7 +22,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # df.to_pickle('./cleaneddf.pkl')
 
 df = pd.read_pickle(r"C:\Users\ekopen\PycharmProjects\pythonProject\cleaneddf.pkl")
-df_focus = df[(df['Year'] >= 2000) & (df['Year'] <= 2005)]
+df_focus = df[(df['Year'] >= 1955) & (df['Year'] <= 2009)]
 dfgrouped = df_focus.groupby('Year').agg('sum').reset_index()
 dfgrouped['Profit Margin'] = dfgrouped['Profit (in millions)'] / dfgrouped['Revenue (in millions)']
 
@@ -33,30 +33,33 @@ for x in dfgrouped['Year']:
     year_dict['DF'] = df_focus[df_focus['Year'] == x]
     year_dict['Average PM'] = np.average(year_dict['DF']['Profit Margin'], weights=year_dict['DF']['Revenue (in millions)'])
     year_dict['Std Dev PM'] = np.std(year_dict['DF']['Profit Margin'])
-    year_dict['Upper Bound'] = (year_dict['Std Dev PM'] + year_dict['Std Dev PM'] * 3)
-    year_dict['Lower Bound'] = (year_dict['Std Dev PM'] + year_dict['Std Dev PM'] * -3)
+    year_dict['Upper Bound'] = (year_dict['Std Dev PM'] + year_dict['Std Dev PM'] * 2)
+    year_dict['Lower Bound'] = (year_dict['Std Dev PM'] + year_dict['Std Dev PM'] * -2)
     bound_test = []
     for y in range(len(year_dict['DF']['Profit Margin'])):
         if (year_dict['DF'].iloc[y,5] > year_dict['Upper Bound']) or (year_dict['DF'].iloc[y,5] < year_dict['Lower Bound']):
             bound_test.append(0)
         else:
             bound_test.append(1)
-    year_dict['DF']['Bounds'] = bound_test
+    year_dict['DF']['In Bounds?'] = bound_test
     df_dict[x] = year_dict
 
-print(df_dict[2001])
+filtereddf = pd.DataFrame()
+for keys in df_dict:
+    filtereddf = filtereddf.append(df_dict[keys]['DF'], ignore_index=True)
+filtereddf = filtereddf[filtereddf['In Bounds?'] == 1]
+filtereddf = filtereddf.groupby('Year').agg('sum').reset_index()
+filtereddf['Profit Margin'] = filtereddf['Profit (in millions)'] / filtereddf['Revenue (in millions)']
 
-dftest = df_dict[2001]['DF'].sort_values(by=['Profit Margin'], ascending=False)
+x2 = filtereddf['Year']
+y3 = filtereddf['Profit Margin']
+z = np.polyfit(x2, y3, 1)
+p = np.poly1d(z)
 
-# x2 = df['Year']
-# y3 = df['Profit Margin']
-# z = np.polyfit(x2, y3, 1)
-# p = np.poly1d(z)
-#
-# plt.plot(x2, y3, label="Profit Margin")
-# plt.plot(x2, p(x2), label="Profit Margin")
-# plt.xlabel("Year")
-# plt.ylabel("%")
-# plt.title("F500 Profit Margin Over Time")
-# plt.show()
+plt.plot(x2, y3, label="Profit Margin")
+plt.plot(x2, p(x2), label="Profit Margin")
+plt.xlabel("Year")
+plt.ylabel("%")
+plt.title("F500 Profit Margin Over Time")
+plt.show()
 
